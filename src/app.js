b80f4bf3e0e20f6bf99967b47dd6ead62be422f5
codingwithkat <codingwithkat@users.noreply.github.com>
2020-06-06 19:27:50 -0400

    Rearranged CSS layout with comments, added icon link to JS and adjusted  the img src placement

--- src/app.js
+++ src/app.js
@@ -1,6 +1,3 @@
-let apiKey = "09fda6f90b159be94949753225d9045d";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
-// console.log(apiUrl);
 
 function formatDate(timestamp) {
     let date = new Date(timestamp);
@@ -18,7 +15,6 @@ function formatDate(timestamp) {
 }
 
 function showTodaysStats(response) {
-    // console.log(response.data.weather[0].description);
     document.querySelector("#selected-city").innerHTML =
         response.data.name;
     document.querySelector("#temperature").innerHTML =
@@ -33,7 +29,13 @@ function showTodaysStats(response) {
         response.data.weather[0].description;
     document.querySelector("#date").innerHTML =
         formatDate(response.data.dt * 1000);
-    // console.log(response.data.dt);
+    document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
+let apiKey = "09fda6f90b159be94949753225d9045d";
+let city = "Las Vegas";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
 axios.get(apiUrl).then(showTodaysStats); //keep outside of function 
+
+
